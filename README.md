a0fc4bf0c0655ad6b39aa11b021091c4bfcb4804
Biswajit Kabi <biswajitkabi711@gmail.com>
2025-02-21 23:04:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is made by Biswajit Kabi
+This project will help you make a customised 3D shirt of your choice.
+You can change the colour, shade, have your personalised picture as well as logo in it. 
+You even have an option to make a AI driven custom shirt of your own choice
---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+import CustomButton from '/src/components/CustomButton';
+import AIPicker from '/src/components/AIPicker';
+import ColorPicker from '/src/components/ColorPicker';
+import FilePicker from '/src/components/FilePicker';
+import Tab from '/src/components/Tab';
+export {CustomButton,
+AIPicker,
+ColorPicker,
+FilePicker,
+Tab};
